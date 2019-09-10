de99cc9fb348aa704bb30672d82dae95cd04daa1
tacaro <tristan.caro@colorado.edu>
2019-09-10 17:37:25 -0600

    can now catch values that arent ints and sequester them, does not disrupt stats

--- get_column_stats.py
+++ get_column_stats.py
@@ -28,17 +28,25 @@ def main():
     mean = "N/A"
     stdev = "N/A"
     V = []
+    baddies = []
 
     for l in f:
-        A = [x for x in l.split()] # splits rows into individual elements
-        V.append(int(A[col_num]))
+        try:
+            A = [x for x in l.split()]  # splits rows into individual elements
+            V.append(int(A[col_num]))
+        except ValueError:
+            baddies.append(A[col_num])
+            continue
         print(V)
 
     if len(V) != 0:
         mean = sum(V)/len(V)
         stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
     else:
-        print("Length of V is zero!")
+        print("No workable values!")
+    if len(baddies) != 0:
+        print("The following values are not integers and were excluded:")
+        print(baddies)
 
     print('mean:', mean)
     print('stdev:', stdev)
