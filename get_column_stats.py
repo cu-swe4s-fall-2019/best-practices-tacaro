ec38add73b81dcf3cd7101dd05130d795e21df9a
tacaro <tristan.caro@colorado.edu>
2019-09-10 09:41:49 -0600

    removed zerodiv issue

--- get_column_stats.py
+++ get_column_stats.py
@@ -2,22 +2,22 @@ import sys
 import math
 import argparse
 
-def Main():
-    print("testing")
+
+def main():
     parser = argparse.ArgumentParser(
                 description='Open a file, generate stats of a given column',
                 prog='bay')
 
-    parser.add_argument('-i' #input file, from user
+    parser.add_argument('-i',  # input file, from user
                         '--input_file',
                         type=str,
                         help="File Name: Tab Separated Files",
                         required=True)
 
-    parser.add_argument('-c' #desired column, from user
+    parser.add_argument('-c',  # desired column, from user
                         '--column_number',
                         type=int,
-                        help='Column number: which column would you like to read?',
+                        help='Column number: which column to read?',
                         required=True)
 
     args = parser.parse_args()
@@ -25,16 +25,24 @@ def Main():
     col_num = args.column_number
 
     f = open(file_name, 'r')
-
+    mean = "N/A"
+    stdev = "N/A"
     V = []
 
     for l in f:
-        A = [int(x) for x in l.split()]
-        V.append(A[col_num])
+        A = [x for x in l.split()] # splits rows into individual elements
+        V.append(int(A[col_num]))
+        print(V)
 
-    mean = sum(V)/len(V)
-
-    stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    if len(V) != 0:
+        mean = sum(V)/len(V)
+        stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    else:
+        print("Length of V is zero!")
 
     print('mean:', mean)
     print('stdev:', stdev)
+
+
+if __name__ == "__main__":
+    main()
