9aaeb8331a26f0eb40c00d2b14b257f4f6aa08e0
tacaro <tristan.caro@colorado.edu>
2019-09-10 18:29:15 -0600

    added two more tests

--- basics_test.sh
+++ basics_test.sh
@@ -6,7 +6,7 @@ pycodestyle get_column_stats.py
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
-python get_column_stats.py --input_file data.txt --column_num 2
+python get_column_stats.py --input_file data.txt --column_num 1
 
 
 V=1
@@ -15,3 +15,13 @@ V=1
 done )> data.txt
 
 python get_column_stats.py --input_file data.txt --column_num 2
+
+A=4
+B="asdf"
+(for i in `seq 1 100`; do 
+    echo "$RANDOM\t$A\t$B\t$A\t$B\t$A\t$RANDOM";
+done )> data.txt
+
+python get_column_stats.py --input_file data.txt --column_num 2
+python get_column_stats.py --input_file data.txt --column_num 1
+python get_column_stats -i nonexistentfile.txt -c 1
