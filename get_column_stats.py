07312c525acbecc0b2431bd24eb232ff07a72273
tacaro <tristan.caro@colorado.edu>
2019-09-10 18:28:22 -0600

    added docstrings, renamed some variables to be user-readable, conformed to PEP8

--- get_column_stats.py
+++ get_column_stats.py
@@ -3,6 +3,41 @@ import math
 import argparse
 
 
+def mean(V):
+    """
+    Compute the arithmetic mean of an array
+    Parameters
+    ----------
+    V : list of int
+        Non-empty array containing values of which to compute mean
+
+    Returns
+    --------
+    m
+        The arithmetic mean of array V
+    """
+    sum(V)/len(V)
+    return m
+
+
+def stdev(V):
+    """
+    The standard deviation of a list of values
+    Parameters
+    -----------
+    V : list of int
+        Non-empty array containing values of which to compute stdev
+
+    Returns
+    --------
+    std
+        Standard deviation of the values in V
+
+    """
+    math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    return std
+
+
 def main():
     parser = argparse.ArgumentParser(
                 description='Open a file, generate stats of a given column',
@@ -24,23 +59,23 @@ def main():
     file_name = args.input_file
     col_num = args.column_number
 
-    try:
+    try:  # first, we need to make sure the file exists/permitted
         f = open(file_name, 'r')
     except FileNotFoundError:
         print("File does not exist, or you do not have correct permissions.")
-        sys.exit(1)
+        sys.exit(1)  # sets the exit code!
 
-    mean = "N/A"
+    mean = "N/A"  # setting N/A to default value
     stdev = "N/A"
     V = []
-    baddies = []
+    bad_vals = []
 
     for l in f:
         try:
             A = [x for x in l.split()]  # splits rows into individual elements
             V.append(int(A[col_num]))
         except ValueError:
-            baddies.append(A[col_num])
+            bad_vals.append(A[col_num])  # we're dumping bad vals into a list
             continue
         print(V)
 
@@ -49,9 +84,9 @@ def main():
         stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
     else:
         print("No workable values!")
-    if len(baddies) != 0:
+    if len(bad_vals) != 0:
         print("The following values are not integers and were excluded:")
-        print(baddies)
+        print(bad_vals)
 
     print('mean:', mean)
     print('stdev:', stdev)
