88856162e27f891f0817f8e7906dabe53ed7697f
tacaro <tristan.caro@colorado.edu>
2019-09-17 21:31:32 -0600

    undid class method, created setUp method

--- basics_test.py
+++ basics_test.py
@@ -1,14 +1,26 @@
 import get_column_stats
 import unittest
+import os
+import random
 
-tArray = [1,2,3,4,5]
+test_array = [1, 2, 3, 4, 5]
+test_array_std = 1.4142135623730951
 
 class TestGet_Column_Stats(unittest.TestCase):
-    def test_mean(self):
-        self.assertEqual(get_column_stats.mean(tArray), 3)
+    def setUp(self):
+        test_array = [1, 2, 3, 4, 5]
+        test_array_std = 1.4142135623730951
+        self.test_file_name = 'generic_name.txt'
+        f = open(self.test_file_name, 'w') # what does the W do?
 
-    def test_stdev(self):
-        self.assertEqual(get_column_stats.stdev(tArray), 1.4142135623730951)
+        
+
+    def test_mean_constant(self):
+        self.assertEqual(get_column_stats.mean(test_array), 3)
+
+    def test_stdev_constant(self):
+        self.assertEqual(get_column_stats.stdev(test_array), test_array_std)
+
 
 if __name__ == '__main__':
     unittest.main()
